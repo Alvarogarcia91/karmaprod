5667b1707bcf6d0d838e966593644dd46f135ae2
Alvaro Garcia <mac@AlvaroMac-17756.local>
2019-11-29 00:42:19 -0600

    created urls karmaapp and views

--- karmaapp/views.py
+++ karmaapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    return HttpResponse('hello world')
+    #return render(request,'index.html')
